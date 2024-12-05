5cda349f6252d994ab7b016360a68524bed992b2
Anhyojin <anhj32118@gmail.com>
2024-12-05 17:32:52 +0900

    24.12.05 - 관리자용 객실 등록 (안효진)
    관리자 프로젝트
    -room/controller/RoomController
    - room/model/vo/Room,RoomImg
    - room/model/dao/RoomDao
    - room/model/service/RoomServiceImpl
    - room/model/service/RoomService
    - resources/mappers/room-mapper.xml
    - views/room/roomEnrollForm, roomList

--- Ski_admin/src/main/java/com/kh/admin/room/model/vo/RoomImg.java
+++ Ski_admin/src/main/java/com/kh/admin/room/model/vo/RoomImg.java
@@ -1,3 +1,76 @@
+package com.kh.admin.room.model.vo;
+
+public class RoomImg {
+	
+	private int roomImgNo;
+	private String roomOriginName;
+	private String roomChangeName;
+	private String roomImgPath;
+	private String roomThumbnail;
+	
+	private int roomNo;  // roomNo 필드 추가
+	
+	public RoomImg() {}
+
+	
+
+	public RoomImg(int roomImgNo, String roomOriginName, String roomChangeName, String roomImgPath,
+			String roomThumbnail, int roomNo) {
+		super();
+		this.roomImgNo = roomImgNo;
+		this.roomOriginName = roomOriginName;
+		this.roomChangeName = roomChangeName;
+		this.roomImgPath = roomImgPath;
+		this.roomThumbnail = roomThumbnail;
+		this.roomNo = roomNo;
+	}
+
+
+
+	public int getRoomImgNo() {
+		return roomImgNo;
+	}
+
+	public void setRoomImgNo(int roomImgNo) {
+		this.roomImgNo = roomImgNo;
+	}
+
+	public String getRoomOriginName() {
+		return roomOriginName;
+	}
+
+	public void setRoomOriginName(String roomOriginName) {
+		this.roomOriginName = roomOriginName;
+	}
+
+	public String getRoomChangeName() {
+		return roomChangeName;
+	}
+
+	public void setRoomChangeName(String roomChangeName) {
+		this.roomChangeName = roomChangeName;
+	}
+
+	public String getRoomImgPath() {
+		return roomImgPath;
+	}
+
+	public void setRoomImgPath(String roomImgPath) {
+		this.roomImgPath = roomImgPath;
+	}
+
+	public String getRoomThumbnail() {
+		return roomThumbnail;
+	}
+
+	public void setRoomThumbnail(String roomThumbnail) {
+		this.roomThumbnail = roomThumbnail;
+	}
+	
+	
+
+	public int getRoomNo() {
+		return roomNo;
 	}
 
 
